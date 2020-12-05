78f17f36f44dc4e0291d38de5c0ca73840939043
Humberto Chaves Junior <hchaves@id.uff.br>
2020-12-05 19:57:37 +0000

    Trabalho completamente implementado

--- main.cs
+++ main.cs
@@ -1,29 +1,37 @@
 using System;
 
-
+public class car{
+  public string name;
+}
+ 
 class MainClass {
   public static void Main (string[] args) {
     Pilha p = new Pilha();
     Console.WriteLine(p.empty());
-    Console.WriteLine(p.top());
-    p.push(10);
-    p.push(20);
-    p.push(30);
-    p.push(40);
-    p.lt();
-    Console.WriteLine("\n");
-    Console.WriteLine(p.top());
-    Console.WriteLine("\n");
-    p.pop();
-    Console.WriteLine("\n");
-    Console.WriteLine(p.top());
-    Console.WriteLine("\n");
-    p.pop(2); 
-    Console.WriteLine(p.top());
+    if(p.top()==null)
+      Console.WriteLine("null");
     
-    Console.WriteLine("\n");
-    p.pop(2); 
-    Console.WriteLine(p.top());
+
+    car c = new car();
+    c.name = "relampago";
+
+    car d = new car();
+    d.name = "mate";
+
+    car e = new car();
+    e.name = "velinho";
+
+    p.push(c);
+    p.push(d);
+    p.push(e);
+    
+    car x = new car();
+    p.pop(2);
+    x = (car)p.top();
+
+    Console.WriteLine(x.name);
+    Console.WriteLine(p.empty());
+
 
   }
 }
--- pilha.cs
+++ pilha.cs
@@ -3,27 +3,27 @@ using System.Collections.Generic;
 
 public class Pilha {
   public static int TAM_MAX = 1000;
-  private List<int> valores;
-  private int topo;
+  private List<object> valores;
+  private object topo;
 
     public Pilha(){
-      this.topo = -1;
-      this.valores = new List<int>();
+      this.topo = null;
+      this.valores = new List<object>();
     }
 
     public bool empty(){
-      if( this.topo == -1) return true;
+      if( this.topo == null) return true;
       else return false;
     }
 
-    public void push(int n){
+    public void push(object n){
       if(this.valores.Count < Pilha.TAM_MAX){
       this.valores.Add(n);
       this.topo = n;
       }
     }
 
-    public int top(){
+    public object top(){
       return this.topo;
     }
 
@@ -32,7 +32,7 @@ public class Pilha {
       if(x > 0){
         this.valores.RemoveAt(x-1);
         if(x==1)
-          this.topo = -1;
+          this.topo = null;
         else
           this.topo = valores[x-2];
       }
